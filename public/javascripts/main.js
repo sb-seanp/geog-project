2edacf4fbe5485424af43ddaa14aaa8ab1a07391
Sean Prasertsit <sean@prasertsit.com>
2015-05-31 13:48:53 -0700

    Finished bus. Waiting on POI.

---
+++ public/javascripts/main.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Sean on 5/31/2015.
+ */
+$(document).ready(function() {
+    $('#fifteenx').change(function () {
+        $('#fifteenx-map').toggleClass('hide');
+    });
+    $('#sixeleven').change(function() {
+        $('#sixeleven-map').toggleClass('hide');
+    });
+    $('#twentyfourx').change(function() {
+        $('#twentyfourx-map').toggleClass('hide');
+    });
+    $('#twentyseven').change(function() {
+        $('#twentyseven-map').toggleClass('hide');
+    });
+});
